709fd2317f8a0fc9f595d14800dfc24484a847da
Divine Threepwood <DivineThreepwood@gmail.com>
2016-04-18 02:22:34 +0200

    upgrade install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
 
-echo Update... &&
-git pull &&
-echo Start installation routine... &&
+#!/bin/bash
+APP_NAME='openhab-binding-rsb'
+
+clear &&
+echo "=== clean ${APP_NAME} ===" &&
+mvn clean $@ &&
+clear &&
+echo "=== install and deploy ${APP_NAME} ===" &&
 mvn clean install -Dopenhab.distribution=$prefix/share/openhab/distribution $@ &&
-echo Installation successful.
+clear &&
+echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
 
