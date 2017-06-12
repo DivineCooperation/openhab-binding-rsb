0ac392643b612bfb3502da342c672c620202d81b
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-06-12 13:07:05 +0200

    install script optimized by skipping licensing.

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
-
 #!/bin/bash
 APP_NAME='openhab-binding-rsb'
-
 clear &&
 echo "=== clean ${APP_NAME} ===" &&
 mvn clean $@ &&
 clear &&
 echo "=== install and deploy ${APP_NAME} ===" &&
-mvn clean install -DskipTests -Dopenhab.distribution=$prefix/share/openhab/distribution $@ &&
+mvn install \
+        -Dopenhab.distribution=$prefix/share/openhab/distribution \
+        -DassembleDirectory=${prefix} \
+        -DskipTests=true \
+        -Dmaven.test.skip=true \
+        -Dlicense.skipAddThirdParty=true \
+        -Dlicense.skipUpdateProjectLicense=true \
+        -Dlicense.skipDownloadLicenses \
+        -Dlicense.skipCheckLicense=true \
+        -Dmaven.license.skip=true $@ &&
+
 clear &&
 echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
 
